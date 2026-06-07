cb3984d13067c8c2142ec8834494c92d8f72ae10
Will Stone <654103+will-stone@users.noreply.github.com>
2026-06-07 12:40:01 +0100

    Fix broken test

--- oxlint-rule-checker/utils/get-our-oxlint-config-with-all-rulesets/get-our-oxlint-config-with-all-rulesets.test.ts
+++ oxlint-rule-checker/utils/get-our-oxlint-config-with-all-rulesets/get-our-oxlint-config-with-all-rulesets.test.ts
@@ -1,6 +1,7 @@
 import { expect, test, vi } from 'vitest'
 
 import reactRuleset from '../../../packages/oxlint-ruleset-react/src/index.ts'
+import tailwindRuleset from '../../../packages/oxlint-ruleset-tailwind/src/index.ts'
 import * as OxlintConfig from '../../../packages/oxlint/src/index.ts'
 import { getOurOxlintConfigWithAllPluginsOn } from './get-our-oxlint-config-with-all-rulesets.ts'
 
@@ -12,6 +13,6 @@ test('should turn on all plugins', () => {
   getOurOxlintConfigWithAllPluginsOn()
 
   expect(oxlintConfigSpy).toHaveBeenCalledWith({
-    rulesets: [reactRuleset()],
+    rulesets: [reactRuleset(), tailwindRuleset()],
   })
 })
